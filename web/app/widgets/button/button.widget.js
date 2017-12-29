d56498237fe75cb67c58beb4fdbab494c5e64c21
Lucky Mallari <luckymallari@gmail.com>
2017-12-28 17:07:20 -0800

    Made the button widget navigable and smarter * Changed "Navigate to dashboard" to "Navigate" * Change Icons uitab to Display * Added ability to navigate to an external URL * Changed ui-select on dashboard select tag * Use/mimic Dummy widget for Navigate type * Added ability to show an item value

--- web/app/widgets/button/button.widget.js
+++ web/app/widgets/button/button.widget.js
@@ -37,8 +37,8 @@
             element[0].parentElement.parentElement.className += " activefeedback";
         }
     }
-    ButtonController.$inject = ['$rootScope', '$scope', '$location', 'OHService'];
-    function ButtonController($rootScope, $scope, $location, OHService) {
+    ButtonController.$inject = ['$rootScope', '$scope', '$location', 'OHService', '$window'];
+    function ButtonController($rootScope, $scope, $location, OHService, $window) {
         var vm = this;
         this.widget = this.ngModel;
 
@@ -57,6 +57,35 @@
             }
         }
 
+        function onNavigate() {
+            if (!vm.widget.navigate_url)
+                return;
+
+            if (vm.widget.navigate_type === 'dashboard') {
+                $location.url('/view/' + vm.widget.navigate_url);
+                return;
+            }
+
+            switch (vm.widget.navigate_target || 'self') {
+                case 'new_tab':
+                    var w = $window.open(vm.widget.navigate_url);
+                    w && (w.opener = null);
+                    break;
+
+                case 'new_window':
+                    var w = $window.open(vm.widget.navigate_url, "_blank", "resizable=1", true);
+                    w && (w.opener = null);
+                    break;
+
+                default:
+                case 'self': {
+                    $window.location.href = vm.widget.navigate_url;
+                    break;
+                }
+            }
+
+        }
+
         OHService.onUpdate($scope, vm.widget.item, function () {
             updateValue();
         });
@@ -64,10 +93,9 @@
         vm.sendCommand = function () {
             switch (vm.widget.action_type) {
                 case "navigate":
-                    if (vm.widget.navigate_dashboard) {
-                        $location.url('/view/' + vm.widget.navigate_dashboard);
-                    }
+                    onNavigate();
                     break;
+
                 case "toggle":
                     if (vm.widget.command && vm.widget.command_alt) {
                         if (vm.value === vm.widget.command) {
@@ -103,7 +131,6 @@
             action_type: widget.action_type || 'command',
             command: widget.command,
             command_alt: widget.command_alt,
-            navigate_dashboard: widget.navigate_dashboard,
             background: widget.background,
             foreground: widget.foreground,
             font_size: widget.font_size,
@@ -116,7 +143,11 @@
             icon: widget.icon,
             icon_size: widget.icon_size,
             icon_nolinebreak: widget.icon_nolinebreak,
-            icon_replacestext: widget.icon_replacestext
+            icon_replacestext: widget.icon_replacestext,
+            show_item_value: widget.show_item_value || false,
+            navigate_type: widget.navigate_type || 'dashboard',
+            navigate_url: widget.navigate_url,
+            navigate_target: widget.navigate_target || 'self'
         };
 
         $scope.dismiss = function () {
@@ -132,16 +163,19 @@
             angular.extend(widget, $scope.form);
             switch (widget.action_type) {
                 case "navigate":
-                    delete widget.item;
                     delete widget.command;
                     delete widget.command_alt;
                     break;
                 case "toggle":
-                    delete widget.navigate_dashboard;
+                    delete widget.navigate_url;
+                    delete widget.navigate_type;
+                    delete widget.navigate_target;
                     break;
                 default:
                     delete widget.command_alt;
-                    delete widget.navigate_dashboard;
+                    delete widget.navigate_url;
+                    delete widget.navigate_type;
+                    delete widget.navigate_target;
                     delete widget.action_type;
                     break;
             }
